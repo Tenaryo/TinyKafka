9ea083bd0726be861431ca728ee15a685954c667
Tenaryo <tylertan@sjtu.edu.cn>
2026-05-10 05:37:53 +0000

    feat: add DescribeTopicPartitions (key=75) to supported APIs

--- src/protocol/api_registry.hpp
+++ src/protocol/api_registry.hpp
@@ -4,6 +4,7 @@
 
 #include "protocol/response.hpp"
 
-inline constexpr std::array<ApiVersionEntry, 1> kSupportedApis{{
+inline constexpr std::array<ApiVersionEntry, 2> kSupportedApis{{
     {.api_key = 18, .min_version = 0, .max_version = 4},
+    {.api_key = 75, .min_version = 0, .max_version = 0},
 }};
